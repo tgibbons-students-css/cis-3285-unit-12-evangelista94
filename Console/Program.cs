9233816a51b71bcae7128b150f54bbee8ccfdf7d
evangelista94 <echicheko@css.edu>
2016-11-22 11:04:33 -0600

    Added a User Repository with email address

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,13 +13,23 @@ namespace ConsoleSample
         static void Main(string[] args)
         {
 
-            EntityRepository entityRepository = new EntityRepository();
+            IEntityRepository<Entity> entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
             myEntity.Name = "Tom";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
 
+
+
+            IEntityRepository<User> UserRepository = new UserRepository();
+            User myUser = UserRepository.GetByID(Guid.NewGuid());
+
+            myUser.Name = "Eva";
+            myUser.EmailAddress = "echicheko@css.edu";
+            Console.WriteLine("New entity ID: " + myUser.ID.ToString());
+            Console.WriteLine("New entity Name: " + myUser.Name);
+            Console.WriteLine("New entity EmailAddress: " + myUser.EmailAddress);
         }
     }
 }
